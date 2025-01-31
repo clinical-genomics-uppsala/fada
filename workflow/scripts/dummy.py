7bdfeaab8a8e29cf6b75edd357f6a28174fa914c
padraicc <padraic.corcoranscilifelab.uu.se>
2025-01-31 13:31:38 +0100

    test: add dummy test until units tests are generated for pipeline scripts

---
+++ workflow/scripts/dummy.py
@@ -0,0 +1,11 @@
+# vim: syntax=python tabstop=4 expandtab
+# coding: utf-8
+
+__author__ = "Pádraic Corcoran"
+__copyright__ = "Copyright 2024, Pádraic Corcoran"
+__email__ = "padraic.corcoran@sciliflab.uu.se"
+__license__ = "GPL-3"
+
+
+def dummy():
+    return 1
